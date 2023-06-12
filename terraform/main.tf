6a55b47828287cbe3657b2d26ed79673c8c25358
parry1981 <136029661+parry1981@users.noreply.github.com>
2023-06-12 17:31:13 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
+    storage_account_name = "terraformcodeops1"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
   }
